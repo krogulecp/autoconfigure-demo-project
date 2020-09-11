82bb4c59b5ce181476ded80dec4f52a06b122342
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-09-11 14:48:39 +0200

    init

---
+++ src/main/java/info/practice/autoconfiguredemoproject/AutoconfigureDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package info.practice.autoconfiguredemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoconfigureDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoconfigureDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/practice/autoconfiguredemoproject/AutoconfigureDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package info.practice.autoconfiguredemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoconfigureDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
