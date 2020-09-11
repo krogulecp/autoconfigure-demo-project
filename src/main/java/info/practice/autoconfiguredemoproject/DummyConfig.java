abde9a474f4ba9e393db63065d03be014e0e6c6b
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-09-11 15:26:57 +0200

    Task III - conditional on missing bean

---
+++ src/main/java/info/practice/autoconfiguredemoproject/DummyConfig.java
@@ -0,0 +1,12 @@
+package info.practice.autoconfiguredemoproject;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DummyConfig {
+    @Bean
+    String dummyString(){
+        return "dummy string in my app";
+    }
+}
