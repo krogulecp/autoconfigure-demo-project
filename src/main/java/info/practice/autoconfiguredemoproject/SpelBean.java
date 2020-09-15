c0638b5341520fe84d294ee55b7e7f0f3810f29e
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-09-15 22:02:09 +0200

    Task VII - conditionalOnExpression

---
+++ src/main/java/info/practice/autoconfiguredemoproject/SpelBean.java
@@ -0,0 +1,12 @@
+package info.practice.autoconfiguredemoproject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpelBean {
+    private String magicx;
+
+    public String getMagicx() {
+        return magicx;
+    }
+}
