70436903a55820efc5bd1db0f1addb4f3fe3472a
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-09-19 12:56:43 +0200

    task 5

--- src/main/java/info/practice/autoconfiguredemoproject/DummyConfiguration.java
+++ src/main/java/info/practice/autoconfiguredemoproject/DummyConfiguration.java
@@ -3,10 +3,8 @@ package info.practice.autoconfiguredemoproject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class DummyConfiguration {
 
-    @Bean
     String dummyString(){
         return "dummy string from app";
     }
---
+++ src/main/java/info/practice/autoconfiguredemoproject/ImportantBean.java
@@ -0,0 +1,6 @@
+package info.practice.autoconfiguredemoproject;
+
+import org.springframework.stereotype.Component;
+
+public class ImportantBean {
+}
