c6b3f43193911354d9b02e5be51c2e3aab644d18
Piotr Krogulec <piotr.krogulec@allegro.pl>
2021-01-16 15:08:32 +0100

    props config demo

--- src/main/java/info/practice/autoconfiguredemoproject/SampleConfig.java
+++ src/main/java/info/practice/autoconfiguredemoproject/SampleConfig.java
@@ -1,7 +1,5 @@
 package info.practice.autoconfiguredemoproject;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +10,7 @@ public class SampleConfig {
 
     public SampleConfig(ApplicationContext applicationContext, MyProps myProps) {
         this.applicationContext = applicationContext;
-        final Object dummyString = applicationContext.getBean("dummyString");
+        final Object dummyString = applicationContext.getBean("myCustomizableAutoconfigurationBean");
         System.out.println(dummyString);
     }
 }
